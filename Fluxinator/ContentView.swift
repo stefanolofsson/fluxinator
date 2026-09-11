15cc395b4acbf178618325629591de7efe5c54d9
Stefan Olofsson <stefan@olofsson.org>
2026-09-11 16:29:40 +0200

    Initial Commit

---
+++ Fluxinator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fluxinator
+//
+//  Created by Stefan Olofsson on 2026-09-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fluxinator/FluxinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxinatorApp.swift
+//  Fluxinator
+//
+//  Created by Stefan Olofsson on 2026-09-11.
+//
+
+import SwiftUI
+
+@main
+struct FluxinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
